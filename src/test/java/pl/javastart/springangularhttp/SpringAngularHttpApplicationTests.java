6bd712f5ec0cb1e68b69ca5709bb2d4b9219c348
Lipien <adam@pawtel.pl>
2019-11-04 18:46:41 +0100

    First commit: backend.

---
+++ src/test/java/pl/javastart/springangularhttp/SpringAngularHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.springangularhttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularHttpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
